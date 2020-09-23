b06dedf4febdc923f68e55d0f88e152e98ede421
cdelange <casey.delange@gmail.com>
2020-09-23 00:45:36 -0500

    Deleted unused code

--- yahoobot.js
+++ yahoobot.js
@@ -23,8 +23,6 @@ let tokens = null;
 
 const yf = new YahooFantasy(yahooAppId, yahooAuthCode);
 
-// yf.setUserToken("NCUnF6afphpsns1v.ZCSceUVB9fr.etwBQOB4DgS7HkSylHNFbr8ROEprjkHgv3fEh4d5hwXLYXcYyteX_m9yKTj3.BLC70g7iRPqe81s4Fzgh4QGmJQatzsATpj_9ExRADV9KYTOHESeEVHQY1W6mWsVgGIWKY_HD6ezyswTVtwFJILJbD04KC8vv6h3VzSZOy8A3rFVAgxQVAtgrQKNC7zA4sO2osTOlG5O9vjdmA7L8Az5zJzh6iP3.14cPKuGFxiKiEtHWtkEoIUlssNBR.l6O4mYDMFi.g8MEfTePo93oxUq877tjvWt.lmI5GEgryRXSAd.w_iEYtrUXnHOzw.ZSZkQtFOPwxJ1LBEODlBJfZ2sQggJsMnMVd5h6BvNv0dXgm5jtzhMRB1cmN3kbSyhh8sxh.Mx3FqY9WZB_I3TGngjlv6KQrgkkiVcD609XOLaRmyqH.s.AENezIYoqj9SLjz.RxPVxJPR9hyhIY8TvdKifJH6Iw1N98ko9RaF2k2D2dmoQy6ps0H4JOqHzoEEHZM5qAEzTPG5ya3igK0lEN8h5vSTO6BBH4rfqwrnCSXal4QJW2tca.usxxEg3DFSInLMFZbb18AOkRzJGzzP_3QSrIPeG3BWyyilDK1B0c_MdrDNuRFmBQLzPWFcxBuUuj0oqWiJ1PKQCWTx0ux8U_lUikxIdyRakW4ql4l2qdPSYaxv8nRvs0FCC4yJEE8dX2iHau1jqm_9LeopxJ0c2ihwsbQC3jLHo5iZh0yixUY8MG2qONeLpi_oikuxlAaQq14FZVYbmv9hdB0AsTmpL.sPtO6c9bBipQmR6d.JF8xwP1QSEmcGCzs3C2Dphb6NiEgE6pFI8IJY_cjdxLecC87wc3qnyxjMo51IdITF5dFI_wnyiIx4m2ghqg7kzQufwFDO8wwKWIItPAezFT4Vbzbd6AH4uuV7AmSKXi3OVwYLV9OYnJ_0gTz00Jk9ne_ajpyb3M.1ihtxS1AkoFMlnxmNQRPYKcHX7PClTrQGD7N");
-
 const getData = async () => {
   try {
     yf.setUserToken(tokens.access_token);
@@ -57,18 +55,6 @@ const getData = async () => {
     console.log(err);
   }
 };
-// try {
-//     // Read credentials file or get new authorization token
-//     await readCredentials();
-
-//     // If crededentials exist
-//     if (tokens) {
-//       const res = await makeAPIrequest(url);
-//     }
-//   } catch (err) {
-//     console.error(`Error in getData(): ${err}`);
-//   }
-// };
 
 // Read tokens.json
 async function readCredentials() {
@@ -136,49 +122,6 @@ function getInitialAuthorization () {
         });
 }
 
-// async function importTokens() {
-//   try {
-//     tokens = await fs.readFile("tokens.json")
-//   } catch {
-//     console.error('Error: ', error.message);
-//   }
-// };
-
-
-// readTokens(function (err, data) {
-//   tokens = JSON.parse(data);
-//   console.log(tokens);
-// });
-
-//  // Hit the Yahoo Fantasy API
-//   async function makeAPIrequest(url) {
-//     let response;
-//     try {
-//       response = await axios({
-//         url,
-//         method: "get",
-//         headers: {
-//           Authorization: `Bearer ${access_token}`,
-//           "Content-Type": "application/x-www-form-urlencoded",
-//           "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.109 Safari/537.36",
-//         },
-//       });
-//       const jsonData = JSON.parse(parser.toJson(response.data));
-//       return jsonData;
-//     } catch (err) {
-//       if (err.response.data && err.response.data.error && err.response.data.error.description && err.response.data.error.description.includes("token_expired")) {
-//         const newToken = await this.refreshAuthorizationToken(this.CREDENTIALS.refresh_token);
-//         if (newToken && newToken.data && newToken.data.access_token) {
-//           this.CREDENTIALS = newToken.data;
-//           this.writeToFile(JSON.stringify(newToken.data), CONFIG.AUTH_FILE, "w");
-//           return this.makeAPIrequest(url, newToken.data.access_token, newToken.data.refresh_token);
-//         }
-//       } else {
-//         console.error(`Error with credentials in makeAPIrequest()/refreshAuthorizationToken(): ${err}`);
-//       }
-//       return err;
-//     }
-//   }
 
 exports.getData = getData;
 exports.readCredentials = readCredentials;
