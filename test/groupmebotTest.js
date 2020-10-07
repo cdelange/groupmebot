940aeb6a23bdc652a1934ab51d8f5409af1148bd
cdelange <casey.delange@gmail.com>
2020-10-06 19:09:45 -0500

    Added Groupmebot.js tests

--- test/groupmebotTest.js
+++ test/groupmebotTest.js
@@ -1,7 +1,5 @@
 const assert = require("chai").assert;
 const commandListener = require("../groupmebot").commandListener;
-const helpMessage = require("../groupmebot").helpMessage;
-
 
 describe("GroupMeBot", function () {
   it("commandListener should return true", function () {
@@ -14,9 +12,5 @@ describe("GroupMeBot", function () {
     assert.isFalse(result, '"What time is it?" is not a command');
   });
 
-  it("helpMessage should return a string", function () {
-    let result = helpMessage();
-    assert.typeOf(result, 'string');
-  });
 
 });
